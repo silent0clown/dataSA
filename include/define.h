85dcf5781d1679f98f318338b8510e503ad9525b
silent0clown <cheng_xiaoyu@163.com>
2023-09-06 23:10:00 +0800

    增加宏定义

--- include/define.h
+++ include/define.h
@@ -5,13 +5,67 @@
 extern "C" {
 #endif
 
+
+#pragma GCC system_header   // 允许可变参数宏
 #define CHECK_NULL_RETURN_VALUE(PTR, VAL){if(NULL == PTR){return VAL;}}
+#define  MAX(x,y) ((x)>(y)) ? (x):(y))  
+#define  MIN(x,y) ((x)<(y)) ? (x):(y)) 
 
 #define    RET_ERROR      (-1)
 #define    RET_SUCCESS    (0)
 #define    RET_OVERFLOW   (9)
 #define    RET_PARA_NULL  (10)
 #define    RET_PARA_FAIL  (11)
+
+
+typedef  unsigned char      u_int8;       /* Unsigned 8  bit value */  
+typedef  unsigned short     u_int16;      /* Unsigned 16 bit value */  
+typedef  unsigned int       u_int32;      /* Unsigned 32 bit value */  
+typedef  unsigned long long u_int64;      /* Unsigned 32 bit value */  
+typedef  signed char        int8;        /* Signed 8  bit value */  
+typedef  signed short       int16;       /* Signed 16 bit value */  
+typedef  signed int         int32;       /* Signed 32 bit value */  
+typedef  signed long long   int64;       /* Signed 32 bit value */  
+  
+/* 另外一种写法，同样很方便清晰 */  
+  
+typedef  unsigned char       BYTE;           /* Unsigned 8  bit value type. */  
+typedef  unsigned short      WORD16;         /* Unsinged 16 bit value type. */  
+typedef  unsigned int        WORD32;         /* Unsigned 32 bit value type. */  
+typedef  unsigned long long  WORD64;         /* Unsigned 64 bit value type. */  
+
+typedef  char                S_BYTE;         /* Signed 8  bit value type. */  
+typedef  short               S_WORD16;       /* Singed 16 bit value type. */  
+typedef  int                 S_WORD32;       /* Signed 32 bit value type. */ 
+typedef  long long           S_WORD64;       /* Signed 64 bit value type. */ 
+
+
+#define DEBUGMSG_OPEN    1  
+  
+#define MYLOGO "[DEBUG INFO] "  
+  
+#define FLAG_FILENAME    1  
+#define FLAG_FUNCTION    1  
+#define FLAG_LINE        1
+  
+#define DSA_PRINT_MSG(fmt,args...)               \  
+{                                                \  
+    if(DEBUGMSG_OPEN){                           \  
+        printf(MYLOGO);                          \  
+                                                 \  
+        if(FLAG_FILENAME)                        \  
+            printf("%s ",__FILE__);              \  
+                                                 \  
+        if(FLAG_FILENAME)                        \  
+            printf("%s ",__FUNCTION__);          \  
+                                                 \  
+        if(FLAG_FILENAME)                        \  
+            printf("%d: ",__LINE__);             \  
+                                                 \  
+        printf(fmt,##args);                      \  
+    }                                            \  
+}  
+
 
 #ifdef __cplusplus
 }
--- src/SeqList/SeqList.h
+++ src/SeqList/SeqList.h
@@ -34,12 +34,57 @@ RET_STATUS InitSeqList(SeqList* List);
 RET_STATUS FreeSeqList(SeqList* List);
 
 RET_STATUS ClearSeqList(SeqList* List);
-
+// 增
 RET_STATUS InsertSeqListElem(SeqList* List, unsigned int i, ElemType Elem);
 
-unsigned int GetSeqListNum(SeqList List);
+// 删
 
+unsigned int GetSeqListNum(SeqList List);
+// 查
 RET_STATUS GetSeqListElem(SeqList List, unsigned int i, ElemType* Elem);
+
+// 改
+/*
+ * 取值
+ *
+ * 获取顺序表中第i个元素，将其存储到e中。
+ * 如果可以找到，返回OK，否则，返回ERROR。
+ *
+ *【备注】
+ * 教材中i的含义是元素位置，从1开始计数，但这不符合编码的通用约定。
+ * 通常，i的含义应该指索引，即从0开始计数。
+ */
+// Status GetElem(SqList L, int i, ElemType* e);
+/*
+ * ████████ 算法2.6 ████████
+ *
+ * 查找
+ *
+ * 返回顺序表中首个与e满足Compare关系的元素位序。
+ * 如果不存在这样的元素，则返回0。
+ *
+ *【备注】
+ * 元素e是Compare函数第二个形参
+ */
+// int LocateElem(SqList L, ElemType e, Status(Compare)(ElemType, ElemType));
+
+/*
+ * 前驱
+ *
+ * 获取元素cur_e的前驱，
+ * 如果存在，将其存储到pre_e中，返回OK，
+ * 如果不存在，则返回ERROR。
+ */
+// Status PriorElem(SqList L, ElemType cur_e, ElemType* pre_e);
+
+/*
+ * 后继
+ *
+ * 获取元素cur_e的后继，
+ * 如果存在，将其存储到next_e中，返回OK，
+ * 如果不存在，则返回ERROR。
+ */
+// Status NextElem(SqList L, ElemType cur_e, ElemType* next_e);
 
 #ifdef __cplusplus
 }
